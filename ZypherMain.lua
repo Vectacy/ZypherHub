3b457456af5f7b169fca6866a5c65556630bb904
Vectacy <vectacy@gmail.com>
2025-03-08 00:41:02 +0800

    Update ZypherMain.lua

--- ZypherMain.lua
+++ ZypherMain.lua
@@ -1,1 +1,3 @@
-
+if game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Vectacy/ZypherHub/refs/heads/main/BF-ZypherHub.lua"))()
+end
